41aedd5062d97f4278dcaa87b41b7a40532147e8
Bob Shens <kiranmanikonda123@gmail.com>
2023-07-01 18:52:09 -0700

    Updated Db

--- client/src/defs.js
+++ client/src/defs.js
@@ -1,4 +1,4 @@
-//const url = 'https://localhost:7157/';
+const url = 'https://localhost:7157/';
 
 // previous deploy
 //const url = 'https://ticketserver.azurewebsites.net/';
@@ -6,6 +6,6 @@
 // new deploy with container
 //const url = 'https://jolly-grape-3wpw.kmanikon-soundbytes.zeet.app/';
 
-const url = 'https://bugtrackerkm.onrender.com/';
+//const url = 'https://bugtrackerkm.onrender.com/';
 
 export default url;
