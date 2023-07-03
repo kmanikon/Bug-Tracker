94823f67df401d8f4a5be5ec4416a6ea3291d462
Bob Shens <kiranmanikonda123@gmail.com>
2023-07-03 04:42:43 -0700

    cleaning up comments

--- client/src/defs.js
+++ client/src/defs.js
@@ -4,16 +4,9 @@
 //const url = 'http://localhost:5251/';
 //const url = 'http://localhost:80/';
 
-// previous deploy
-//const url = 'https://ticketserver.azurewebsites.net/';
-
-// new deploy with container
-//const url = 'https://jolly-grape-3wpw.kmanikon-soundbytes.zeet.app/';
-
-//const url = 'https://bugtrackerkm.onrender.com/';
 
 // Google Cloud url
-// make sure new image pushes are not cached
+// make sure new image pushes are not cached (new image on dockerhub)
 const url = 'https://bugtrackerkm-7ebk6ir5ca-uw.a.run.app/';
 
 export default url;
