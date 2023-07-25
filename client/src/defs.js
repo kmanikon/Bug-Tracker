5f2efbb8dbf95af7c70f35dbe740550b82822f0e
Bob Shens <kiranmanikonda123@gmail.com>
2023-07-25 01:49:26 -0700

    Updated URL

--- client/src/defs.js
+++ client/src/defs.js
@@ -1,12 +1,12 @@
 // local urls for dev server
 
 //const url = 'https://localhost:7157/';
-const url = 'http://localhost:5251/';
+//const url = 'http://localhost:5251/';
 //const url = 'http://localhost:80/';
 
 
 // Google Cloud url
 // make sure new image pushes are not cached (new image on dockerhub)
-//const url = 'https://bugtracker-nuckuqerya-uc.a.run.app/'; 
+const url = 'https://bugtracker-nuckuqerya-uc.a.run.app/'; 
 
 export default url;
